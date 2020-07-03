b38c9a02fdffaaf3e9f6fcf5d13331f32ef82cbc
HirotoMaezato <maezatohiroto1031@gmail.com>
2020-07-03 06:41:12 +0000

    コミットメッセージ

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -106,9 +106,23 @@
       margin-top: 10px;
       margin-right: 20px;
     }
+    
+    .editor {
+      margin-top: 47px;
+    }
   
 </style>
 <div class="create-blade-php">
+  
+  <!--<div class="editor">-->
+  <!--       <span class="text-muted">作成者</span>-->
+  <!--       <p><a href="{{ action('UsersController@index') }}" class="text-secondary"><small>&#64;{{Auth::user()->name}}</small></a></p>-->
+  <!--</div>-->
+  <div class="editor">
+         <span class="text-muted">作成者</span>
+         <p><a href="#" class="text-secondary"><small>&#64;{{$posts->user->name}}</small></a></p>
+  </div>
+  
   <div class="form">
     <div class="form-group form-group1">
       <div class="contents1">
